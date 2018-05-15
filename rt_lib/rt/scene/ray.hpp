2d3999919c7cff610c97d22a74325d0ef4c66732
David Ganter <ganterd@tcd.ie>
2018-05-15 11:57:33 +0100

    Forgot to a file

---
+++ rt_lib/rt/scene/ray.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include <rt/scene/mesh/tri.hpp>
+
+namespace rt
+{
+    class Ray
+    {
+    public:
+        glm::vec3 mOrigin;
+        glm::vec3 mDirection;  
+    };
+
+    class Tri;
+    class RayHit
+    {
+    public:
+        RayHit()
+        {
+            mDistance = std::numeric_limits<float>::max();
+        }
+
+        float mDistance;
+        glm::vec3 mHitPosition;
+        glm::vec3 mSurfaceNormal;
+        Tri* mTri;
+    };
+}
