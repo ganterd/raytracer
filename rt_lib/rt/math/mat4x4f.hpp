0e1c9511bab54b125df1cf995e3efc968e616b4d
Dave Ganter <davidganter@gmail.com>
2020-05-23 21:25:37 +0100

    More tests for mat

--- rt_lib/rt/math/mat4x4f.hpp
+++ rt_lib/rt/math/mat4x4f.hpp
@@ -32,20 +32,197 @@ public:
     {
         return diagonal(1.0f);
     };
+
+    static mat4x4f rotation(const float4& eulerAngles)
+    {
+        const float sz = sinf(eulerAngles.z);
+        const float cz = cosf(eulerAngles.z);
+        const float sy = sinf(eulerAngles.y);
+        const float cy = cosf(eulerAngles.y);
+        const float sx = sinf(eulerAngles.x);
+        const float cx = cosf(eulerAngles.x);
+
+        mat4x4f r;
+        r.rows[0] = float4(cz * cy,   cz * sy * sx - sz * cx,   cz * sy * cx + sz * sx,   0.0f);
+        r.rows[1] = float4(sz * cy,   sz * sy * sx + cz * cx,   sz * sy * cx - cz * sx,   0.0f);
+        r.rows[2] = float4(-sy,       cy * sx,                  cy * cx,                  0.0f);
+        r.rows[3] = float4(0.0f,      0.0f,                     0.0f,                     1.0f);
+        return r;
+    }
+
+    inline float& operator[](int i)
+    {
+        return data[i];
+    }
+
+    inline const float operator[](int i) const
+    {
+        return data[i];
+    }
 };
 
-inline float4 operator*(const mat4x4f& lhs, const float4& rhs)
+// inline float4 operator*(const mat4x4f& lhs, const float4& rhs)
+// {
+//     float4 result;
+//     for (int row = 0; row < 4; ++row) {
+//         __m128 acc = _mm_setzero_ps();
+
+//         __m128 matRow = lhs.sseData[row];
+//         acc = _mm_mul_ps(matRow, rhs.sseData);
+//         acc = _mm_hadd_ps(acc, acc);
+//         acc = _mm_hadd_ps(acc, acc);
+
+//         _mm_store_ss(&result[row], acc);
+//     }
+//     return result;
+// }
+
+inline float4 operator*(const float4& lhs, const mat4x4f& rhs)
 {
     float4 result;
     for (int row = 0; row < 4; ++row) {
         __m128 acc = _mm_setzero_ps();
 
-        __m128 matRow = lhs.sseData[row];
-        acc = _mm_mul_ps(matRow, rhs.sseData);
+        __m128 matRow = rhs.sseData[row];
+        acc = _mm_mul_ps(matRow, lhs.sseData);
         acc = _mm_hadd_ps(acc, acc);
         acc = _mm_hadd_ps(acc, acc);
 
         _mm_store_ss(&result[row], acc);
     }
     return result;
+}
+
+inline mat4x4f inverse(const mat4x4f& m)
+{
+    mat4x4f inv;
+    float det;
+    int i;
+
+    inv[0] = m[5]  * m[10] * m[15] - 
+             m[5]  * m[11] * m[14] - 
+             m[9]  * m[6]  * m[15] + 
+             m[9]  * m[7]  * m[14] +
+             m[13] * m[6]  * m[11] - 
+             m[13] * m[7]  * m[10];
+
+    inv[4] = -m[4]  * m[10] * m[15] + 
+              m[4]  * m[11] * m[14] + 
+              m[8]  * m[6]  * m[15] - 
+              m[8]  * m[7]  * m[14] - 
+              m[12] * m[6]  * m[11] + 
+              m[12] * m[7]  * m[10];
+
+    inv[8] = m[4]  * m[9] * m[15] - 
+             m[4]  * m[11] * m[13] - 
+             m[8]  * m[5] * m[15] + 
+             m[8]  * m[7] * m[13] + 
+             m[12] * m[5] * m[11] - 
+             m[12] * m[7] * m[9];
+
+    inv[12] = -m[4]  * m[9] * m[14] + 
+               m[4]  * m[10] * m[13] +
+               m[8]  * m[5] * m[14] - 
+               m[8]  * m[6] * m[13] - 
+               m[12] * m[5] * m[10] + 
+               m[12] * m[6] * m[9];
+
+    inv[1] = -m[1]  * m[10] * m[15] + 
+              m[1]  * m[11] * m[14] + 
+              m[9]  * m[2] * m[15] - 
+              m[9]  * m[3] * m[14] - 
+              m[13] * m[2] * m[11] + 
+              m[13] * m[3] * m[10];
+
+    inv[5] = m[0]  * m[10] * m[15] - 
+             m[0]  * m[11] * m[14] - 
+             m[8]  * m[2] * m[15] + 
+             m[8]  * m[3] * m[14] + 
+             m[12] * m[2] * m[11] - 
+             m[12] * m[3] * m[10];
+
+    inv[9] = -m[0]  * m[9] * m[15] + 
+              m[0]  * m[11] * m[13] + 
+              m[8]  * m[1] * m[15] - 
+              m[8]  * m[3] * m[13] - 
+              m[12] * m[1] * m[11] + 
+              m[12] * m[3] * m[9];
+
+    inv[13] = m[0]  * m[9] * m[14] - 
+              m[0]  * m[10] * m[13] - 
+              m[8]  * m[1] * m[14] + 
+              m[8]  * m[2] * m[13] + 
+              m[12] * m[1] * m[10] - 
+              m[12] * m[2] * m[9];
+
+    inv[2] = m[1]  * m[6] * m[15] - 
+             m[1]  * m[7] * m[14] - 
+             m[5]  * m[2] * m[15] + 
+             m[5]  * m[3] * m[14] + 
+             m[13] * m[2] * m[7] - 
+             m[13] * m[3] * m[6];
+
+    inv[6] = -m[0]  * m[6] * m[15] + 
+              m[0]  * m[7] * m[14] + 
+              m[4]  * m[2] * m[15] - 
+              m[4]  * m[3] * m[14] - 
+              m[12] * m[2] * m[7] + 
+              m[12] * m[3] * m[6];
+
+    inv[10] = m[0]  * m[5] * m[15] - 
+              m[0]  * m[7] * m[13] - 
+              m[4]  * m[1] * m[15] + 
+              m[4]  * m[3] * m[13] + 
+              m[12] * m[1] * m[7] - 
+              m[12] * m[3] * m[5];
+
+    inv[14] = -m[0]  * m[5] * m[14] + 
+               m[0]  * m[6] * m[13] + 
+               m[4]  * m[1] * m[14] - 
+               m[4]  * m[2] * m[13] - 
+               m[12] * m[1] * m[6] + 
+               m[12] * m[2] * m[5];
+
+    inv[3] = -m[1] * m[6] * m[11] + 
+              m[1] * m[7] * m[10] + 
+              m[5] * m[2] * m[11] - 
+              m[5] * m[3] * m[10] - 
+              m[9] * m[2] * m[7] + 
+              m[9] * m[3] * m[6];
+
+    inv[7] = m[0] * m[6] * m[11] - 
+             m[0] * m[7] * m[10] - 
+             m[4] * m[2] * m[11] + 
+             m[4] * m[3] * m[10] + 
+             m[8] * m[2] * m[7] - 
+             m[8] * m[3] * m[6];
+
+    inv[11] = -m[0] * m[5] * m[11] + 
+               m[0] * m[7] * m[9] + 
+               m[4] * m[1] * m[11] - 
+               m[4] * m[3] * m[9] - 
+               m[8] * m[1] * m[7] + 
+               m[8] * m[3] * m[5];
+
+    inv[15] = m[0] * m[5] * m[10] - 
+              m[0] * m[6] * m[9] - 
+              m[4] * m[1] * m[10] + 
+              m[4] * m[2] * m[9] + 
+              m[8] * m[1] * m[6] - 
+              m[8] * m[2] * m[5];
+
+    det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+
+    if (det == 0)
+    {
+        return m;
+    }
+
+    det = 1.0 / det;
+    for (i = 0; i < 16; i++)
+    {
+        inv[i] = inv[i] * det;
+    }
+
+    return inv;
 }
--- rt_lib_test/math/mat4x4f.cpp
+++ rt_lib_test/math/mat4x4f.cpp
@@ -18,7 +18,7 @@ TEST(mat4x4f, mul_float4_identity)
     mat4x4f m = mat4x4f::identity();
     float4 v(1, 2, 3, 4);
 
-    float4 r = m * v;
+    float4 r = v * m;
 
     EXPECT_EQ(r, v);
 }
@@ -28,7 +28,175 @@ TEST(mat4x4f, mul_float4_scale)
     mat4x4f m = mat4x4f::diagonal(2.0f);
     float4 v(1, 2, 3, 4);
 
-    float4 r = m * v;
+    float4 r = v * m;
 
     EXPECT_EQ(r, float4(2, 4, 6, 8));
+}
+
+/**
+ * Expect a 0-degree rotation to leave a vector unchanged
+ */
+TEST(mat4x4f, rotate_none)
+{
+    float toRadians = M_PI / 180.0f;
+    mat4x4f m = mat4x4f::rotation(float4(0.0f, 0.0f, 0.0f, 0.0f));
+    float4 v(0.0f, 0.0f, 1.0f, 0.0f);
+
+    v = v * m;
+
+    float4 expected(0.0f, 0.0f, 1.0f, 0.0f);
+
+    // This is where FP32 precision issues might start happenning
+    EXPECT_EQ(v, expected);
+}
+
+/**
+ * Expect a 90 degree rotation around the x-axis.
+ */
+TEST(mat4x4f, create_rotation_x)
+{
+    float r = 90.0f * (M_PI / 180.0f);
+    mat4x4f m = mat4x4f::rotation(float4(r, 0.0f, 0.0f, 0.0f));
+
+    mat4x4f expected;
+    expected.rows[0] = float4(1.0f, 0.0f, 0.0f, 0.0f);
+    expected.rows[1] = float4(0.0f, cosf(r), -sinf(r), 0.0f);
+    expected.rows[2] = float4(0.0f, sinf(r),  cosf(r), 0.0f);
+    expected.rows[3] = float4(0.0f, 0.0f, 0.0f, 1.0f);
+
+    for(int i = 0; i < 16; ++i) EXPECT_FLOAT_EQ(m.data[i], expected.data[i]);
+}
+
+/**
+ * Expect a 90 degree rotation around the y-axis.
+ */
+TEST(mat4x4f, create_rotation_y)
+{
+    float r = 90.0f * (M_PI / 180.0f);
+    mat4x4f m = mat4x4f::rotation(float4(0.0f, r, 0.0f, 0.0f));
+
+    mat4x4f expected;
+    expected.rows[0] = float4(cosf(r), 0.0f, sinf(r), 0.0f);
+    expected.rows[1] = float4(0.0f, 1.0f, 0.0f, 0.0f);
+    expected.rows[2] = float4(-sinf(r), 0.0f,  cosf(r), 0.0f);
+    expected.rows[3] = float4(0.0f, 0.0f, 0.0f, 1.0f);
+
+    for(int i = 0; i < 16; ++i) EXPECT_FLOAT_EQ(m.data[i], expected.data[i]);
+}
+
+/**
+ * Expect a 90 degree rotation around the y-axis.
+ */
+TEST(mat4x4f, create_rotation_z)
+{
+    float r = 90.0f * (M_PI / 180.0f);
+    mat4x4f m = mat4x4f::rotation(float4(0.0f, 0.0f, r, 0.0f));
+
+    mat4x4f expected;
+    expected.rows[0] = float4(cosf(r), -sinf(r), 0.0f, 0.0f);
+    expected.rows[1] = float4(sinf(r), cosf(r), 0.0f, 0.0f);
+    expected.rows[2] = float4(0.0f, 0.0f,  1.0f, 0.0f);
+    expected.rows[3] = float4(0.0f, 0.0f, 0.0f, 1.0f);
+
+    for(int i = 0; i < 16; ++i) EXPECT_FLOAT_EQ(m.data[i], expected.data[i]);
+}
+
+/**
+ * Expect a 90 degree rotation around the x-axis.
+ */
+TEST(mat4x4f, rotate_x)
+{
+    float r = 90.0f * (M_PI / 180.0f);
+    mat4x4f m = mat4x4f::rotation(float4(r, 0.0f, 0.0f, 0.0f));
+    float4 v(0.0f, 0.0f, 1.0f, 0.0f);
+
+    v = v * m;
+
+    float4 expected(0.0f, -1.0f, 0.0f, 0.0f);
+
+    // This is where FP32 precision issues might start happenning
+    const float smol_number = 0.0000001f;
+    EXPECT_NEAR(v.x, expected.x, smol_number);
+    EXPECT_NEAR(v.y, expected.y, smol_number);
+    EXPECT_NEAR(v.z, expected.z, smol_number);
+    EXPECT_NEAR(v.w, expected.w, smol_number);
+}
+
+/**
+ * Expect a 90 degree rotation around the y-axis.
+ */
+TEST(mat4x4f, rotate_y)
+{
+    float r = 90.0f * (M_PI / 180.0f);
+    mat4x4f m = mat4x4f::rotation(float4(0.0f, r, 0.0f, 0.0f));
+    float4 v(0.0f, 0.0f, 1.0f, 0.0f);
+
+    v = v * m;
+
+    float4 expected(1.0f, 0.0f, 0.0f, 0.0f);
+
+    // This is where FP32 precision issues might start happenning
+    const float smol_number = 0.0000001f;
+    EXPECT_NEAR(v.x, expected.x, smol_number);
+    EXPECT_NEAR(v.y, expected.y, smol_number);
+    EXPECT_NEAR(v.z, expected.z, smol_number);
+    EXPECT_NEAR(v.w, expected.w, smol_number);
+}
+
+/**
+ * Expect a 90 degree rotation around the z-axis.
+ */
+TEST(mat4x4f, rotate_z)
+{
+    float r = 90.0f * (M_PI / 180.0f);
+    mat4x4f m = mat4x4f::rotation(float4(0.0f, 0.0f, r, 0.0f));
+    float4 v(0.0f, 1.0f, 0.0f, 0.0f);
+
+    v = v * m;
+
+    float4 expected(-1.0f, 0.0f, 0.0f, 0.0f);
+
+    // This is where FP32 precision issues might start happenning
+    const float smol_number = 0.0000001f;
+    EXPECT_NEAR(v.x, expected.x, smol_number);
+    EXPECT_NEAR(v.y, expected.y, smol_number);
+    EXPECT_NEAR(v.z, expected.z, smol_number);
+    EXPECT_NEAR(v.w, expected.w, smol_number);
+}
+
+TEST(mat4x4f, rotate_xy)
+{
+    float rx = 45.0f * (M_PI / 180.0f);
+    float ry = 90.0f * (M_PI / 180.0f);
+    mat4x4f mx = mat4x4f::rotation(float4(rx, 0.0f, 0.0f, 0.0f));
+    mat4x4f my = mat4x4f::rotation(float4(0.0f, ry, 0.0f, 0.0f));
+    float4 v(1.0f, 0.0f, 0.0f, 0.0f);
+
+    //v = mx * my * v;
+    v = v * my * mx;
+
+    float4 expected(0.0f, 0.707107f, -0.707107f, 0.0f);
+
+    // This is where FP32 precision issues might start happenning
+    const float smol_number = 0.000001f;
+    EXPECT_NEAR(v.x, expected.x, smol_number);
+    EXPECT_NEAR(v.y, expected.y, smol_number);
+    EXPECT_NEAR(v.z, expected.z, smol_number);
+    EXPECT_NEAR(v.w, expected.w, smol_number);
+}
+
+TEST(mat4x4f, rotate_rotate_inverse)
+{
+    float toRadians = M_PI / 180.0f;
+    float angle = 90.0f * toRadians;
+    mat4x4f m = mat4x4f::rotation(float4(0.0f, 0.0f, angle, 0.0f));
+    mat4x4f m_inv = inverse(m);
+
+    float4 v(0.0f, 1.0f, 0.0f, 0.0f);
+
+    float4 actual = v * m;
+    actual = actual * m_inv;
+
+    // This is where FP32 precision issues might start happenning
+    EXPECT_EQ(actual, v);
 }
